ed5912c999d0d4fb2105f13ef09f315b744b0a30
zolkin.u.a <zolkin.u.a@gmail.com>
2025-03-05 15:46:39 +0200

    Configure yaml language server

--- nvim/ftplugin/yaml.lua
+++ nvim/ftplugin/yaml.lua
@@ -1,1 +1,31 @@
+MiniDeps.later(function()
+    print("Processed: after/ftplugin/yaml.lua")
 
+    local lspconfig = require("lspconfig")
+    lspconfig.yamlls.setup({
+        settings = {
+            redhat = { telemetry = { enabled = false } },
+            yaml = {
+                keyOrdering = false,
+                format = {
+                    enable = false,
+                },
+                validate = true,
+                schemaStore = {
+                    -- Must disable built-in schemaStore support to use
+                    -- schemas from SchemaStore.nvim plugin
+                    enable = false,
+                    -- Avoid TypeError: Cannot read properties of undefined (reading 'length')
+                    url = "",
+                },
+                schemas = {
+                    kubernetes = { "k8s**.yaml", "kube*/*.yaml", "tshoot*/*.yaml" },
+                    ["https://json.schemastore.org/kustomization.json"] = "kustomization.{yml,yaml}",
+                    ["https://raw.githubusercontent.com/docker/compose/master/compose/config/compose_spec.json"] = "docker-compose*.{yml,yaml}",
+                    ["https://raw.githubusercontent.com/datreeio/CRDs-catalog/main/argoproj.io/application_v1alpha1.json"] = "*.Application.yaml",
+                    ["https://raw.githubusercontent.com/datreeio/CRDs-catalog/main/argoproj.io/applicationset_v1alpha1.json"] = "*.ApplicationSet.yaml",
+                },
+            },
+        },
+    })
+end)
--- nvim/lua/plugins/mason.lua
+++ nvim/lua/plugins/mason.lua
@@ -1,7 +1,14 @@
 MiniDeps.add("williamboman/mason.nvim")
-MiniDeps.later(
-	function()
-		local mason = require('mason')
-		mason.setup({})
-	end
-)
+MiniDeps.later(function()
+    local mason = require("mason")
+    mason.setup({
+        ensure_installed = {
+            "stylua",
+            "lua-language-server",
+            "yaml-language-server",
+        },
+        ui = {
+            border = "rounded",
+        },
+    })
+end)
