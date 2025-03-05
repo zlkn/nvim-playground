18e6001493971d607e643e6e1aecb89239443cdb
zolkin.u.a <zolkin.u.a@gmail.com>
2025-03-05 13:03:23 +0200

    setup stylua

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,10 +1,10 @@
 local path_package = vim.fn.stdpath("data") .. "/site/"
 local mini_path = path_package .. "pack/deps/start/mini.nvim"
 if not vim.loop.fs_stat(mini_path) then
-	vim.cmd('echo "Installing `mini.nvim`" | redraw')
-	local clone_cmd = { "git", "clone", "--filter=blob:none", "https://github.com/echasnovski/mini.nvim", mini_path }
-	vim.fn.system(clone_cmd)
-	vim.cmd('echo "Installed `mini.nvim`" | redraw')
+    vim.cmd('echo "Installing `mini.nvim`" | redraw')
+    local clone_cmd = { "git", "clone", "--filter=blob:none", "https://github.com/echasnovski/mini.nvim", mini_path }
+    vim.fn.system(clone_cmd)
+    vim.cmd('echo "Installed `mini.nvim`" | redraw')
 end
 
 -- Set up 'mini.deps' (customize to your liking)
